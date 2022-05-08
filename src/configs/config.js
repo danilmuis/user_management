ce2727ff698a26a1f82da5bc76741a6a547d0d60
Muhammad Danil Muis <daniltelkom@gmail.com>
2022-05-08 15:15:52 +0700

    kubernetes yaml done

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,10 +2,10 @@ const path = require('path')
 
 const dotenv = require('dotenv')
 
-const result = dotenv.config({ path : path.resolve(process.cwd(), '.env')});
+// const result = dotenv.config({ path : path.resolve(process.cwd(), '.env')});
 
-if (result.error) {
-    throw result.error;
-}
+// if (result.error) {
+//     throw result.error;
+// }
 
 module.exports = process.env;
